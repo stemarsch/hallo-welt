2f467b7e1e67a8c96d61fe437a25445befd39a78
Markuese <m.schmidt@ifhkoeln.de>
2023-06-22 16:28:19 +0200

    za.py

--- za.py
+++ za.py
@@ -1,1 +1,13 @@
+def addiere_zahlen(a, b):
+    summe = a + b
+    return summe
 
+# Benutzereingaben erhalten
+zahl1 = float(input("Gib die erste Zahl ein: "))
+zahl2 = float(input("Gib die zweite Zahl ein: "))
+
+# Zahlen addieren
+ergebnis = addiere_zahlen(zahl1, zahl2)
+
+# Ergebnis ausgeben
+print("Die Summe ist:", ergebnis)
